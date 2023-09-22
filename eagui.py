84bee2d72b12cd31ef6a824b06f9045baaf1fe68
Isac Sélea <isac.selea@husqvarnagroup.com>
2023-09-22 14:35:21 +0200

    text input sanitation

--- eagui.py
+++ eagui.py
@@ -28,38 +28,50 @@ SETV_BUTTON_X =             0.585
 SETV_BUTTON_Y =             0.205
 SETA_BUTTON_X =             0.585
 SETA_BUTTON_Y =             0.305
+
 ON_BUTTON_X =               0.2
 ON_BUTTON_Y =               0.6
 OFF_BUTTON_X =              0.5
 OFF_BUTTON_Y =              0.6
+
 REMOTE_ON_BUTTON_X =        0.2
 REMOTE_ON_BUTTON_Y =        0.7
 REMOTE_OFF_BUTTON_X =       0.5
 REMOTE_OFF_BUTTON_Y =       0.7
+
 LABEL_VOLT_X =              0.7
 LABEL_VOLT_Y =              0.01
 VOLT_VAL_X =                0.11
 VOLT_VAL_Y =                0.015
+
 LABEL_CURRENT_X =           0.7
 LABEL_CURRENT_Y =           0.11
 CURRENT_VAL_X =             0.11
 CURRENT_VAL_Y =             0.11
+
 LABEL_STATUS_X =            0.01
 LABEL_STATUS_Y =            0.94
 LABEL_CONNECTION_STATUS_X = 0.65
 LABEL_CONNECTION_STATUS_Y = 0.94
+
 SET_VOLT_TEXTBOX_X =        0.1
 SET_VOLT_TEXTBOX_Y =        0.2
+
 SET_CURRENT_TEXTBOX_X =     0.1
 SET_CURRENT_TEXTBOX_Y =     0.3
+
 SET_OVP_TEXTBOX_X =     0.1
 SET_OVP_TEXTBOX_Y =     0.4
+
 SET_OCP_TEXTBOX_X =     0.1
 SET_OCP_TEXTBOX_Y =     0.5
+
 SET_OVP_BUTTON_X =     0.5889
 SET_OVP_BUTTON_Y =     0.4
+
 SET_OCP_BUTTON_X =     0.5889
 SET_OCP_BUTTON_Y =     0.5
+
 WATT_VAL_X =           0.11
 WATT_VAL_Y =           0.81
 LABEL_WATT_X =         0.7
@@ -124,65 +136,53 @@ class psuData:
 
         self.setVoltageButton = tk.Button(top)
         self.setVoltageButton.place(relx=SETV_BUTTON_X, rely=SETV_BUTTON_Y, height=28, width=80)
-        self.setVoltageButton.configure(text='''Set Voltage''')
-        self.setVoltageButton.configure(command=self.psu_setVolt)
+        self.setVoltageButton.configure(text='''Set Voltage''', command=self.psu_setVolt)
 
         self.setCurrentButton = tk.Button(top)
         self.setCurrentButton.place(relx=SETA_BUTTON_X, rely=SETA_BUTTON_Y, height=28, width=80)
-        self.setCurrentButton.configure(text='''Set Current''')
-        self.setCurrentButton.configure(command=self.psu_setCurrent)
+        self.setCurrentButton.configure(text='''Set Current''', command=self.psu_setCurrent)
 
         self.setOvpButton = tk.Button(top)
         self.setOvpButton.place(relx=SET_OVP_BUTTON_X, rely=SET_OVP_BUTTON_Y, height=28, width=80)
-        self.setOvpButton.configure(text='''Set OVP''')
-        self.setOvpButton.configure(command=self.psu_setOvp)
+        self.setOvpButton.configure(text='''Set OVP''', command=self.psu_setOvp)
 
         self.setOcpButton = tk.Button(top)
         self.setOcpButton.place(relx=SET_OCP_BUTTON_X, rely=SET_OCP_BUTTON_Y, height=28, width=80)
-        self.setOcpButton.configure(text='''Set OCP''')
-        self.setOcpButton.configure(command=self.psu_setOcp)
+        self.setOcpButton.configure(text='''Set OCP''', command=self.psu_setOcp)
 
         self.buttonON = tk.Button(top)
         self.buttonON.place(relx=ON_BUTTON_X, rely=ON_BUTTON_Y, height=28, width=75)
-        self.buttonON.configure(text='''On''')
-        self.buttonON.configure(command=self.power_on)
+        self.buttonON.configure(text='''On''', command=self.power_on)
 
         self.buttonOff = tk.Button(top)
         self.buttonOff.place(relx=OFF_BUTTON_X, rely=OFF_BUTTON_Y, height=28, width=75)
-        self.buttonOff.configure(text='''Off''')
-        self.buttonOff.configure(command=self.power_off)
+        self.buttonOff.configure(text='''Off''', command=self.power_off)
 
         self.buttonRemoteOn = tk.Button(top)
         self.buttonRemoteOn.place(relx=REMOTE_ON_BUTTON_X, rely=REMOTE_ON_BUTTON_Y, height=28, width=75)
-        self.buttonRemoteOn.configure(text='''Connect''')
-        self.buttonRemoteOn.configure(command=self.psu_connect)
+        self.buttonRemoteOn.configure(text='''Connect''', command=self.psu_connect)
 
         self.buttonRemoteOff = tk.Button(top)
         self.buttonRemoteOff.place(relx=REMOTE_OFF_BUTTON_X, rely=REMOTE_OFF_BUTTON_Y, height=28, width=75)
-        self.buttonRemoteOff.configure(text='''Disconnect''')
-        self.buttonRemoteOff.configure(command=self.psu_disconnect)
+        self.buttonRemoteOff.configure(text='''Disconnect''', command=self.psu_disconnect)
 
         self.labelVolt = tk.Label(top)
         self.labelVolt.place(relx=LABEL_VOLT_X, rely=LABEL_VOLT_Y, height=33, width=37)
-        self.labelVolt.configure(font=("Arial", 14))
-        self.labelVolt.configure(text='''V''')
+        self.labelVolt.configure(font=("Arial", 14), text='''V''')
 
         self.labelCurrent = tk.Label(top)
         self.labelCurrent.place(relx=LABEL_CURRENT_X, rely=LABEL_CURRENT_Y, height=33, width=37)
-        self.labelCurrent.configure(font=("Arial", 14))
-        self.labelCurrent.configure(text='''A''')
+        self.labelCurrent.configure(font=("Arial", 14), text='''A''')
 
         self.labelVoltageVal = tk.Label(top)
         self.labelVoltageVal.place(relx=VOLT_VAL_X, rely=VOLT_VAL_Y, height=30, width=100)
         self.voltValue.set(str(self.volt))
-        self.labelVoltageVal.configure(font=("Arial", 14))
-        self.labelVoltageVal.configure(textvariable=self.voltValue)
+        self.labelVoltageVal.configure(font=("Arial", 14), textvariable=self.voltValue)
 
         self.labelCurentVal = tk.Label(top)
         self.labelCurentVal.place(relx=CURRENT_VAL_X, rely=CURRENT_VAL_Y, height=30, width=100)
         self.currValue.set(str(self.current))
-        self.labelCurentVal.configure(font=("Arial", 14))
-        self.labelCurentVal.configure(textvariable=self.currValue)
+        self.labelCurentVal.configure(font=("Arial", 14), textvariable=self.currValue)
 
         self.labelStatus = tk.Label(top)
         self.labelStatus.place(relx=LABEL_STATUS_X, rely=LABEL_STATUS_Y, height=23, width=100)
@@ -192,33 +192,34 @@ class psuData:
         self.labelConnectionStatus.place(relx=LABEL_CONNECTION_STATUS_X, rely=LABEL_CONNECTION_STATUS_Y, height=23, width=100)
         self.labelConnectionStatus.configure(text='''Idle''')
 
+        self.labelWatt = tk.Label(top)
+        self.labelWatt.place(relx=LABEL_WATT_X, rely=LABEL_WATT_Y, height=33, width=37)
+        self.labelWatt.configure(font=("Arial", 14), text='''W''')
+
+        self.labelWattVal = tk.Label(top)
+        self.labelWattVal.place(relx=WATT_VAL_X, rely=WATT_VAL_Y, height=30, width=100)
+        self.wattValue.set(str(self.watt))
+        self.labelWattVal.configure(font=("Arial", 14), textvariable=self.wattValue)
+
         self.setVoltage = tk.Text(top)
         self.setVoltage.place(relx=SET_VOLT_TEXTBOX_X, rely=SET_VOLT_TEXTBOX_Y, relheight=0.071, relwidth=0.4)
-        self.setVoltage.configure(background="white")
-        self.setVoltage.configure(font="TkTextFont")
-        self.setVoltage.configure(foreground="black")
-        self.setVoltage.configure(wrap="word")
+        self.setVoltage.configure(background="white", font="TkTextFont", foreground="black", wrap="none")
+        self.setVoltage.bind("<KeyRelease>", self.on_text_change)
 
         self.setCurrent = tk.Text(top)
         self.setCurrent.place(relx=SET_CURRENT_TEXTBOX_X, rely=SET_CURRENT_TEXTBOX_Y, relheight=0.071, relwidth=0.4)
-        self.setCurrent.configure(background="white")
-        self.setCurrent.configure(font="TkTextFont")
-        self.setCurrent.configure(foreground="black")
-        self.setCurrent.configure(wrap="word")
+        self.setCurrent.configure(background="white", font="TkTextFont", foreground="black", wrap="none")
+        self.setCurrent.bind("<KeyRelease>", self.on_text_change)
 
         self.setOvp = tk.Text(top)
         self.setOvp.place(relx=SET_OVP_TEXTBOX_X, rely=SET_OVP_TEXTBOX_Y, relheight=0.071, relwidth=0.4)
-        self.setOvp.configure(background="white")
-        self.setOvp.configure(font="TkTextFont")
-        self.setOvp.configure(foreground="black")
-        self.setOvp.configure(wrap="word")
+        self.setOvp.configure(background="white", font="TkTextFont", foreground="black", wrap="none")
+        self.setOvp.bind("<KeyRelease>", self.on_text_change)
 
         self.setOcp = tk.Text(top)
         self.setOcp.place(relx=SET_OCP_TEXTBOX_X, rely=SET_OCP_TEXTBOX_Y, relheight=0.071, relwidth=0.4)
-        self.setOcp.configure(background="white")
-        self.setOcp.configure(font="TkTextFont")
-        self.setOcp.configure(foreground="black")
-        self.setOcp.configure(wrap="word")
+        self.setOcp.configure(background="white", font="TkTextFont", foreground="black", wrap="none")
+        self.setOcp.bind("<KeyRelease>", self.on_text_change)
 
         self.setVoltage.delete("1.0", END)
         self.setVoltage.insert("1.0", str(round(self.defaultSetVolt, 3)))
@@ -229,18 +230,7 @@ class psuData:
         self.setOcp.delete("1.0", END)
         self.setOcp.insert("1.0", str(round(self.defaultOcp, 3)))
 
-        self.labelWatt = tk.Label(top)
-        self.labelWatt.place(relx=LABEL_WATT_X, rely=LABEL_WATT_Y, height=33, width=37)
-        self.labelWatt.configure(font=("Arial", 14))
-        self.labelWatt.configure(text='''W''')
-
-        self.labelWattVal = tk.Label(top)
-        self.labelWattVal.place(relx=WATT_VAL_X, rely=WATT_VAL_Y, height=30, width=100)
-        self.wattValue.set(str(self.watt))
-        self.labelWattVal.configure(font=("Arial", 14))
-        self.labelWattVal.configure(textvariable=self.wattValue)
-
-        self.labelConnectionStatus.configure(text="Connected")
+        self.psu_connect()
 
     def destroy_all(self):
         self.status = False
@@ -308,6 +298,13 @@ class psuData:
         psu.set_ocp(setOcp)
         config.set("PSU_PARAMS", "set_ocp", str(setOcp))
 
+    def on_text_change(self, event):
+        text_widget = event.widget
+        text = text_widget.get("1.0", "end-1c")  # Get the current content without the trailing newline
+        new_text = ''.join(c for c in text if c.isdigit() or c == '.')
+        text_widget.delete("1.0", "end")
+        text_widget.insert("1.0", new_text)
+        
 if __name__ == '__main__':
     path = os.path.abspath(__file__)
     os.chdir(os.path.dirname(path))
